470ec2ea4891aa30dab156df15e89c1c935bf0f5
Leo <leonardo.silvestrin@usp.br>
2022-11-29 14:41:42 -0300

    a

--- Simples/crop.py
+++ Simples/crop.py
@@ -1,7 +1,11 @@
 from PIL import Image
 im1 = Image.open('Simples\\Bandeira.PNG').resize((500,350))
 x,y = im1.size
-caixa = (0,0,x/2,y/2)
-croped = im1.crop(caixa)
-croped.paste(im1,(0,0,int(x/2),int(y/2)))
-im1.show()
+
+caixas = [(0,0,int(x/2),int(y/2)),(int(x/2),0,x,int(y/2)), (0,int(y/2),int(x/2),y), (int(x/2),int(y/2),x,y)]
+
+for caixa in caixas:
+    croped = im1.crop(caixa).rotate(180)
+    im1.paste(croped, caixa)
+
+im1.show()  
